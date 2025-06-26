55e46a9938d9bb6fadf83a246a74b2a2d4d9e62a
Rakshitha-D <rakshithagowda.d@gmail.com>
2025-06-26 16:51:29 +0530

    SBCOSS-523: updated karma config to run test cases in ci

--- karma.conf.cjs
+++ karma.conf.cjs
@@ -71,6 +71,7 @@ module.exports = function (config) {
     // start these browsers
     // available browser launchers: https://www.npmjs.com/search?q=keywords:karma-launcher
     browsers: ['ChromeHeadless'], 
+    flags: ['--no-sandbox'],
    
 
 
