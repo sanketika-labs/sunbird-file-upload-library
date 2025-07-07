a65e844917a0e971b0a214075d6a53132248bcb9
Rakshitha-D <rakshithagowda.d@gmail.com>
2025-07-07 12:07:17 +0530

    SBCOSS-523: updated karma config file

--- karma.conf.cjs
+++ karma.conf.cjs
@@ -74,7 +74,7 @@ module.exports = function (config) {
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--disable-gpu']
+        flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage']
       }
     },
 
